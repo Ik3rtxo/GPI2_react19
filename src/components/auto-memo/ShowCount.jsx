dfd6bdbdfd617ce937fdb396fc66dc716320314d
gomez.145978 <gomez.145978@e.unavarra.es>
2025-03-17 20:34:24 +0100

    primer commit

--- src/components/auto-memo/ShowCount.jsx
+++
@@ -1,9 +0,0 @@
-export default function ShowCount({ title, count }) {
-    console.log(`rendering ${title}....`);
-
-    return (
-        <div className="bg-emerald-50 p-5 rounded border border-gray-300">
-            {title} is {count}
-        </div>
-    );
-}
--- src/components/use-hook-promise/Starter.jsx
+++
@@ -1,7 +0,0 @@
-export default function Message() {
-    return (
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Download message
-        </button>
-    );
-}
